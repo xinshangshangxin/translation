9d563d5d42f6a9bc975318d6ad29791eea7382d4
xinshangshangxin <xinshangshangxin@gmail.com>
2015-03-03 12:46:27 +0800

    添加chrome插件版, 修改一个函数名

--- chrome插件版/googletranslater.js
+++ chrome插件版/googletranslater.js
@@ -1,15 +1,3 @@
-// ==UserScript==
-// @name           googletranslate
-// @version        15.03.01
-// @description    谷歌翻译
-// @icon           http://i3.tietuku.com/11d6c35e96ef7c9f.jpg
-// @include        *
-// @grant          GM_xmlhttpRequest
-// @auther         SHANG殇
-// @namespace      SHANG
-// ==/UserScript==
-
-
 var isstart = false;
 var showhtml = null;
 document.addEventListener('keydown', function(e) {
@@ -73,34 +61,62 @@ function init() {
         return Object.prototype.toString.call(obj) === '[object Array]';
     }
 
+    function ajaxPost(url, data, funScuss, funFail) {
+        // 1, 创建 XMLHttpRequest
+        var xhr;
+        if (window.XMLHttpRequest) { //IE7+, Firefox, Chrome, Opera, Safari
+            xhr = new XMLHttpRequest();
+        } else { //IE6, IE5
+            xhr = new ActiveXObject('Microsoft.XMLHTTP');
+        }
+
+        url += url.match(/\?/) ? '&' : '?' + 'time=' + new Date().getTime(); // 防止缓存
+
+        // 设置超时取消
+        setTimeout(function() {
+            xhr.abort();
+            funFail && funFail('timeout');
+        }, 20 * 1000);
+
+        //2 连接服务器
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        // 3, 发送请求
+        xhr.send(data);
+
+        // 4, 接收服务器的返回
+        xhr.onreadystatechange = function() {
+            // 完成返回
+            if (xhr.readyState === 4) {
+                // 成功接收数据
+                if (xhr.status === 200) {
+                    funScuss && funScuss(xhr.responseText);
+                } else {
+                    funFail && funFail(xhr.status);
+                }
+            }
+        };
+    }
+
     function translateByGoogle(str, fun, funFail) {
         var protocol = location.protocol;
         var url = (protocol.match('file') ? 'http:' : protocol)
             // http://translate.google.cn/translate_a/t?client=json&text=do%20not%20do%20this%20%0A%20do%20not%20do%20this&langpair=auto|auto
         var currentPostData = 'client=json&text=' + encodeURIComponent(str) + '&langpair=auto|auto';
-        GM_xmlhttpRequest({
-            method: 'POST',
-            url: url + '//translate.google.cn/translate_a/t',
-            data: currentPostData,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-            onload: function(res) {
-                var json = JSON.parse(res.responseText);
-                var translatearr = json.sentences;
-                var htmlarrsrc = [];
-                var htmlarrdst = [];
-                for (var i = 0; i < translatearr.length; i++) {
-                    htmlarrsrc.push(translatearr[i].orig);
-                    htmlarrdst.push(translatearr[i].trans);
-                }
-                fun && fun(htmlarrsrc, htmlarrdst);
+        ajaxPost(url + '//translate.google.cn/translate_a/t', currentPostData, function(res) {
+            var json = JSON.parse(res);
+            var translatearr = json.sentences;
+            var htmlarrsrc = [];
+            var htmlarrdst = [];
+            for (var i = 0; i < translatearr.length; i++) {
+                htmlarrsrc.push(translatearr[i].orig);
+                htmlarrdst.push(translatearr[i].trans);
             }
+            fun && fun(htmlarrsrc, htmlarrdst);
         });
     }
 
     function showInfo(e) {
-        console.log('123');
         if (showhtml.isstop || e.target.id === 'showhtml_id' || e.target.parentNode && e.target.parentNode.id === 'showhtml_id') {
             return;
         }
